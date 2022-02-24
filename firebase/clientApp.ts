9aadb258925a805af8b5534bbde53ab9cac62b4c
Andre Westerlund <andrewest_04@hotmail.com>
2022-02-24 15:56:12 +1300

    add firebase

---
+++ firebase/clientApp.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+if(!firebase.getApps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
