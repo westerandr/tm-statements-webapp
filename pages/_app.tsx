63ff4daa0c5324fed921d347378dc2c50b27b1b6
Andre Westerlund <andrewest_04@hotmail.com>
2022-03-07 15:51:50 +1300

    wrap Auth Provider around all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../config/theme';
 import createEmotionCache from '../config/createEmotionCache';
+import { AuthUserProvider } from '../lib/context/userContext';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -24,7 +25,7 @@ export default function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+         <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>
       </ThemeProvider>
     </CacheProvider>
   );
