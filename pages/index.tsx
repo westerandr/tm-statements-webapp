abeb06dda9174102bc300cda1a9cd53e997df085
Andre Westerlund <andrewest_04@hotmail.com>
2022-03-08 15:40:06 +1300

    add navbar component and logout logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from './components/Link';
 import Copyright from './components/Copyright';
+import Navbar from './components/Navbar';
 import { useAuth } from '../lib/context/userContext'; 
 import { useRouter } from 'next/router';
 
@@ -13,35 +14,34 @@ const Home: NextPage = () => {
   const { authUser } = useAuth();
   useEffect(() => {
     if (!authUser) {
-      router.push('/login');
+      router.replace('/login');
     } 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (!authUser) {
-    return <div></div>;
-  }
-
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <Copyright />
-      </Box>
-    </Container>
+    <>
+      <Navbar />
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            my: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h4" component="h1" gutterBottom>
+            MUI v5 + Next.js with TypeScript example
+          </Typography>
+          <Link href="/about" color="secondary">
+            Go to the about page
+          </Link>
+          <Copyright />
+        </Box>
+      </Container>
+    </>
   );
 };
 
