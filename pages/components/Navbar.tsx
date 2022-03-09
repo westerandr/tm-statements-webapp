ba6d770724777c2edd40f6561e4a1612c48f790e
Andre Westerlund <andrewest_04@hotmail.com>
2022-03-09 15:04:33 +1300

    refactor navbar component

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -6,22 +6,18 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import Button from '@mui/material/Button';
 import useFirebaseAuth from '../../lib/hooks/useFirebaseAuth';
 import { toast } from 'material-react-toastify';
 
-const pages = ["Orders", "Customers"]
 const settings = ['Logout'];
 
 function Navbar() {
   const { authUser, signOut } = useFirebaseAuth();
   const router = useRouter();
-  const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
 
   const logOut = () => {
@@ -36,20 +32,12 @@ function Navbar() {
     }
   }
 
-  const handleOpenNavMenu = (event: any) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event: any) => {
     setAnchorElUser(event.currentTarget);
     console.log(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
   const handleCloseUserMenu = () => {
-    console.log(anchorElUser);
     setAnchorElUser(null);
   };
   
@@ -64,44 +52,7 @@ function Navbar() {
             sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
           >
             <Avatar sx={{ width: 50, height: 50 }} src="/logo.jpg" />
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          </Typography>          
           <Typography
             variant="h6"
             noWrap
@@ -111,15 +62,6 @@ function Navbar() {
             <Avatar sx={{ width: 50, height: 50 }} src="/logo.jpg" />
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
