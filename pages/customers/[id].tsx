967b636fcedfbd78a4d7e1472ee03dcfb9f2c6c2
Andre Westerlund <andrewest_04@hotmail.com>
2022-03-15 12:37:30 +1300

    add proper display for customer with redemption info

--- pages/customers/[id].tsx
+++ pages/customers/[id].tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { database } from '../../config/firebase';
 import { collection, doc, DocumentData, DocumentReference, DocumentSnapshot, getDoc } from 'firebase/firestore';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { Customer as CustomerType } from '../../lib/types';
+import RedemptionInfo from '../components/RedemptionInfo';
 
 const customersCollection = collection(database, 'users');
 
@@ -13,8 +14,12 @@ type CustomerProps = {
 function Customer({ customer } : CustomerProps) {
 
   return (
-    <Container maxWidth="lg">
-      <h1>{customer?.handle}</h1>
+    <Container sx={{py: '2rem'}} maxWidth="lg">
+      <Typography variant="h1" sx={{my: '1rem', fontSize: '3.5rem'}}>{`${customer.firstName} ${customer.lastName}`}</Typography>
+      <Typography variant="h2" color="secondary" sx={{mb: '1rem', fontSize: '2rem'}}>{`@${customer.handle}`}</Typography>
+      <Typography variant="body1" sx={{mt: '1rem', mb: '5rem', fontSize: '1.5rem'}}>{`Current Points: ${customer.currentPoints}`}</Typography>
+      <Typography variant="h2" sx={{mb: '1rem', fontSize: '2.8rem'}}>Redemption Info</Typography>
+      <RedemptionInfo />
     </Container>
   )
 }
