57ec0b3764acd7a1f6125071ba17b58e7f8c460e
Andre Westerlund <andrewest_04@hotmail.com>
2022-03-15 12:02:55 +1300

    dynamic routing for customer via firestore doc id

--- pages/components/CustomerList.tsx
+++ pages/components/CustomerList.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { database } from '../../config/firebase';
-import { collection } from 'firebase/firestore';
+import { collection, DocumentSnapshot } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { Container, Typography, List, ListItem, ListItemText, ListItemIcon, IconButton, CircularProgress } from '@mui/material';
 import { Customer } from '../../lib/types';
@@ -13,6 +14,11 @@ const customersCollection = collection(database, 'users');
 
 function CustomerList() {
   const [ users, usersloading, userserror ] = useCollection(customersCollection, { });
+  const router = useRouter();
+
+  const viewCustomer = (uid: string) => {
+    router.push(`/customers/${uid}`);
+  }
 
   if(usersloading) {
     return <CircularProgress />
@@ -24,9 +30,10 @@ function CustomerList() {
        
         <List >
         {
-          users && !users.empty && users.docs.map((doc: any) => {
-            const customer: Customer = doc.data();
-          
+          users && !users.empty && users.docs.map((doc: DocumentSnapshot) => {
+            const data : any = doc.data();
+            const customer : Customer = { ...data, uid: doc.id };
+
             return (
           
                 <ListItem key={customer?.uid}>
@@ -35,7 +42,7 @@ function CustomerList() {
                     secondary={`@${customer?.handle}`}
                   />
                   <ListItemIcon>
-                    <IconButton>
+                    <IconButton onClick={() => viewCustomer(doc?.id)}>
                       <PageViewIcon />
                     </IconButton>
                   </ListItemIcon>
--- pages/customers/[id].tsx
+++ pages/customers/[id].tsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React from 'react';
 import { database } from '../../config/firebase';
-import { collection } from 'firebase/firestore';
+import { collection, doc, DocumentData, DocumentReference, DocumentSnapshot, getDoc } from 'firebase/firestore';
+import { Container } from '@mui/material';
+import { Customer as CustomerType } from '../../lib/types';
 
 const customersCollection = collection(database, 'users');
 
-function Customer() {
+type CustomerProps = {
+  customer: CustomerType
+}
+
+function Customer({ customer } : CustomerProps) {
 
   return (
-    <div>
-      <h1>Customer ID</h1>
-    </div>
+    <Container maxWidth="lg">
+      <h1>{customer?.handle}</h1>
+    </Container>
   )
+}
+
+export async function getServerSideProps(context : any) {
+  const { id } = context.query;
+  // get customer document by id
+  const docRef: DocumentReference = doc(customersCollection, id); 
+  const document: DocumentSnapshot<DocumentData> = await getDoc(docRef);
+  const data: any = document.data();
+  const customer: CustomerType = { ...data, uid: document.id };
+
+
+    return {
+      props: {
+        customer
+      }
+    }
+
 }
 
 
