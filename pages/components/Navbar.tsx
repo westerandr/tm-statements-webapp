56ab6c23b7fdc84f9ca862dabfc53362958e69c0
Andre Westerlund <andrewest_04@hotmail.com>
2022-04-01 13:18:11 +1300

    add pages to Navbar and routing functionality

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -6,6 +6,8 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
@@ -15,10 +17,12 @@ import Link from './Link';
 import { toast } from 'material-react-toastify';
 
 const settings = ['Logout'];
+const pages = ['Customers', 'Orders',];
 
 function Navbar() {
   const { authUser, signOut } = useFirebaseAuth();
   const router = useRouter();
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
 
   const logOut = () => {
@@ -33,13 +37,23 @@ function Navbar() {
     }
   }
 
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
   const handleOpenUserMenu = (event: any) => {
     setAnchorElUser(event.currentTarget);
-    console.log(event.currentTarget);
   };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
+  };
+
+  
+  const handleCloseNavMenu = (page: string) => {
+    const href = `/${page.toLowerCase()}`;
+    router.push(href);
+    setAnchorElNav(null);
   };
 
   if(!authUser){
@@ -60,15 +74,62 @@ function Navbar() {
               <Avatar sx={{ width: 50, height: 50 }} src="/logo.jpg" />
             </Link>
           </Typography>          
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={() => handleCloseNavMenu(page)}>
+                    <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
           <Typography
             variant="h6"
             noWrap
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
           >
-            <Avatar sx={{ width: 50, height: 50 }} src="/logo.jpg" />
+             <Link href="/">
+              <Avatar sx={{ width: 50, height: 50 }} src="/logo.jpg" />
+            </Link>
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={() => handleCloseNavMenu(page)}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
